a313bc1e19255b7c6268e46efff69c66b24d5ed2
trapajim <tobsus@tobias-mac.local>
2015-12-04 15:59:13 +0900

    added tab functionality and tab panel

--- libs/tabs.jsx
+++ libs/tabs.jsx
@@ -4,45 +4,126 @@ var React = require('react');
 var classNames = require('classnames');
 
 var Tabs = React.createClass({
-	propTypes: {
-		className: React.PropTypes.oneOfType([
+  propTypes: {
+    className: React.PropTypes.oneOfType([
       React.PropTypes.array,
       React.PropTypes.string,
     ]),
-		activeTab: React.PropTypes.number,
-		onMount: React.PropTypes.func,
-		onBeforeChange: React.PropTypes.func,
+    activeTab: React.PropTypes.number,
+    onMount: React.PropTypes.func,
+    onBeforeChange: React.PropTypes.func,
     onAfterChange: React.PropTypes.func,
     children: React.PropTypes.oneOfType([
       React.PropTypes.array,
       React.PropTypes.element
     ]).isRequired
-	},
-	getDefaultProps(){
+  },
+  getDefaultProps(){
     return {activeTab: 1};
   },
   getInitialState(){
-  	return { activeTab: this.state.activeTab};
+    return { activeTab: this.props.activeTab};
   },
   componentDidMount(){
-  	if(this.props.onMount){
-  		this.onMount().apply(null, this.getCallBackParameter());
-  	}
+    if(this.props.onMount){
+      this.props.onMount.apply(null, this._getCallbackParameter());
+    }
   },
   componentWillReceiveProps(newProps){
-  	if(newProps.activeTab && newProps.activeTab !== this.props.activeTab){
+    if(newProps.activeTab && newProps.activeTab !== this.props.activeTab){
       this.setState({activeTab: newProps.activeTab});
     }
   },
   render(){
-  	var tabContainerClass = classNames('tabs-container',this.props.className)
-  	return(
-  		<div className={tabContainerClass}>
-  		</div>
-  	)
+    var tabContainerClass = classNames('tabs-container',this.props.className)
+    return(
+      <div className={tabContainerClass}>
+        {this._getMenu()}
+        {this._getSelectedPanel()}
+      </div>
+    );
   },
-  getCallBackParameter(){
-  	//index, selected panel, selected menu
-  	return [ this.state.activeTab, this.refs.tab-panel, this.refs.tab-menu-${this.state.activeTab];
+  setActive(index, e){
+    e.preventDefault();
+
+    var callbackParams = this._getCallbackParameter(index);
+    if (this.props.onBeforeChange) {
+      var cancel = this.props.onBeforeChange.apply(null,callbackParams)
+      if(cancel === false){ return }
+    }
+
+    this.setState({ activeTab: index }, () => {
+      if (this.props.onAfterChange) {
+       this.props.onAfterChange.apply(null,callbackParams)
+      }
+    });
+  },
+  _getCallbackParameter(index){
+    //index, selected panel, selected menu
+    var activeTab = typeof index === "undefined" ? this.state.activeTab : index
+    return [ activeTab, this.tabPanel, this['tab-menu-${activeTab}']];
+  },
+  _getMenu(){
+    if (!this.props.children) {
+      throw new Error('children not found, please add Tabs.Panel');
+    }
+
+    if (!Array.isArray(this.props.children)) {
+      this.props.children = [this.props.children];
+    }
+
+    var $items = this.props.children
+      .map($panel => typeof $panel === 'function' ? $panel() : $panel)
+      .filter($panel => $panel)
+      .map(($panel, index) => {
+        var menuClasses = classNames(
+          'tabs-menu-item',
+          this.state.activeTab === (index + 1) && 'is-active'
+        );
+        return (
+          <li ref={(ref) => this['tab-menu-${index + 1}'] = ref} key={index} className={menuClasses}>
+            <a onClick={this.setActive.bind(this, index + 1)}>
+              {$panel.props.title}
+            </a>
+          </li>
+        );
+    });
+    return (
+      <ul className="tab-menu">{$items}</ul>
+    );
+  },
+  _getSelectedPanel () {
+    var panelIndex = this.state.activeTab - 1;
+    return (
+      <div ref={(ref)=>this.tabPanel = ref} className='tab-panel'>
+        {this.props.children[panelIndex]}
+      </div>
+    );
   }
 });
+
+// tab panel
+Tabs.Panel = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string.isRequired,
+    children: React.PropTypes.oneOfType([
+      React.PropTypes.array,
+      React.PropTypes.element
+    ]).isRequired,
+    onPanelMount: React.PropTypes.func
+  },
+  componentDidMount(){
+    if(this.props.onPanelMount){
+      this.props.onMount()
+    }
+  },
+  componentWillReceiveProps(newProps){
+    if(newProps.onPanelMount){
+      newProps.onMount()
+    }
+  },
+  render () {
+    return <div>{this.props.children}</div>;
+  }
+});
+module.exports = Tabs;
